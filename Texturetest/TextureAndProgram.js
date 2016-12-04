9f97cb104b291c1fb18488f9e898425ed740da00
DRN999 <nnakano@ucsc.edu>
2016-12-03 16:56:59 -0800

    imagefix

--- Texturetest/TextureAndProgram.js
+++ Texturetest/TextureAndProgram.js
@@ -132,6 +132,7 @@ function drawThingsTexture()
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 255, 255]));
 
 	var image = new Image();
+	image.crossOrigin = "";
 	image.src = "../resources/f-texture.png";
 	image.addEventListener('load', function() 
 	{
