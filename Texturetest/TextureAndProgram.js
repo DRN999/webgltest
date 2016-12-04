186ffeb71ea674bf8d34983dc26b9b0098893efe
DRN999 <nnakano@ucsc.edu>
2016-12-04 01:48:31 -0800

    images

--- Texturetest/TextureAndProgram.js
+++ Texturetest/TextureAndProgram.js
@@ -61,9 +61,17 @@ function handleTextureLoaded(image, texture)
 	gl.bindTexture(gl.TEXTURE_2D, texture);
 	gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true);
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-	gl.generateMipmap(gl.TEXTURE_2D);
+	if(isPowerOf2(image.width) && isPowerOf2(image.height))\
+	{
+		gl.generateMipmap(gl.TEXTURE_2D);
+	}
+	else
+	{
+		gl.texParameteri(gl.TETXURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TETXURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TETXURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	}
+	
 }
 
 function imageLoadedCheck()
