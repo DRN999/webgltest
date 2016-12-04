65dbab7a91dcaddb47cbf8d01fc00817a42fa89a
DRN999 <nnakano@ucsc.edu>
2016-12-03 19:13:44 -0800

    change to onload

--- Texturetest/TextureAndProgram.js
+++ Texturetest/TextureAndProgram.js
@@ -151,6 +151,7 @@ function initImages()
 	var image = new Image();
 	image.onload = function()
 	{
+		console.log("loaded");
 		handleTextureLoaded(image, texture);
 		gl.activeTexture(gl.TEXTURE0);
 		gl.bindTexture(gl.TEXTURE_2D, texture);
@@ -168,7 +169,7 @@ function handleTextureLoaded(image, texture)
 	gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true);
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 	gl.generateMipmap(gl.TEXTURE_2D);
     gl.bindTexture(gl.TEXTURE_2D, null);
 }
